f2e28ddf9895e3c012d0b363650611fa802d0e59
alalamav <alalamav@users.noreply.github.com>
2018-09-04 17:33:46 -0400

    Windows: resolve DNS over TCP when UDP is not available

--- electron/process_manager.ts
+++ electron/process_manager.ts
@@ -85,10 +85,12 @@ export function startVpn(config: cordova.plugins.outline.ServerConfig, onDisconn
           return validateServerCredentials();
         })
         .then(() => {
-          return checkUdpForwardingEnabled();
-        })
-        .then(() => {
-          return startTun2socks(ip, onDisconnected);
+          return checkUdpForwardingEnabled().then(() => {
+            return startTun2socks(ip, true, onDisconnected);
+          }).catch((e) => {
+            console.warn('UDP forwarding disabled.');
+            return startTun2socks(ip, false, onDisconnected);
+          });
         })
         .then(() => {
           return routingService.configureRouting(TUN2SOCKS_VIRTUAL_ROUTER_IP, ip);
@@ -310,13 +312,15 @@ function getDnsRequest() {
   ]);
 }
 
-function startTun2socks(host: string, onDisconnected: () => void): Promise<void> {
+function startTun2socks(
+    host: string, isUdpForwardingEnabled: boolean, onDisconnected: () => void): Promise<void> {
   return new Promise((resolve, reject) => {
     // ./badvpn-tun2socks.exe \
     //   --tundev "tap0901:outline-tap0:10.0.85.2:10.0.85.0:255.255.255.0" \
     //   --netif-ipaddr 10.0.85.1 --netif-netmask 255.255.255.0 \
     //   --socks-server-addr 127.0.0.1:1081 \
-    //   --socks5-udp --udp-relay-addr 127.0.0.1:1081
+    //   --socks5-udp --udp-relay-addr 127.0.0.1:1081 \
+    //   --transparent-dns
     const args: string[] = [];
     args.push(
         '--tundev',
@@ -325,9 +329,13 @@ function startTun2socks(host: string, onDisconnected: () => void): Promise<void>
     args.push('--netif-ipaddr', TUN2SOCKS_VIRTUAL_ROUTER_IP);
     args.push('--netif-netmask', TUN2SOCKS_VIRTUAL_ROUTER_NETMASK);
     args.push('--socks-server-addr', `${PROXY_IP}:${SS_LOCAL_PORT}`);
-    args.push('--socks5-udp');
-    args.push('--udp-relay-addr', `${PROXY_IP}:${SS_LOCAL_PORT}`);
     args.push('--loglevel', 'error');
+    args.push('--transparent-dns');
+    // Enabling transparent DNS without UDP options causes tun2socks to use TCP for DNS resolution.
+    if (isUdpForwardingEnabled) {
+      args.push('--socks5-udp');
+      args.push('--udp-relay-addr', `${PROXY_IP}:${SS_LOCAL_PORT}`);
+    }
 
     // TODO: Duplicate ss-local's error handling.
     try {
@@ -344,7 +352,7 @@ function startTun2socks(host: string, onDisconnected: () => void): Promise<void>
             // Restart tun2socks with a timeout so the event kicks in when the device wakes up.
             console.info('Restarting tun2socks...');
             setTimeout(() => {
-              startTun2socks(host, onDisconnected)
+              startTun2socks(host, isUdpForwardingEnabled, onDisconnected)
                   .then(() => {
                     resolve();
                   })
